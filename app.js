7dcb1c88fd5de3289a356440211e53786266e97a
1fly1git <banach.d@wp.pl>
2026-07-31 00:39:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -188,6 +188,7 @@ document.getElementById("findForm").style.display = "block";
   );
 }
 async function saveFinding() {
+  alert("saveFinding działa");
   if (!window.supabaseClient) {
     showMessage("Brak połączenia z Supabase.");
     return;
