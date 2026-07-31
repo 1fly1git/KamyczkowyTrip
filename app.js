25762331cd62db7edc2b403327ecd99eaa8277d3
1fly1git <banach.d@wp.pl>
2026-07-31 20:58:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -554,7 +554,7 @@ async function loadStatistics() {
 
     const places = new Set();
     const finders = new Set();
-
+let photosCount = 0;
     data.forEach(function (finding) {
       if (finding.place_name) {
         places.add(
